7ae16e6edbe8b2c5d11e660984b8f95f11a8a60b
Aayush Srivastava <153167411+notaayushsrivastava@users.noreply.github.com>
2026-08-24 21:49:39 +0530

    Add 14_longest_common_prefix.py - LeetCode Problem 14

---
+++ 14_longest_common_prefix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        strs.sort()
+        i = 0
+
+        while i < len(strs[0]) and i < len(strs[-1]) and strs[0][i] == strs[-1][i]:
+            i += 1
+            
+        return strs[0][:i]
